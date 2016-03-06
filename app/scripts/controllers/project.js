62d020f2be4e1b2207d5559382f250abb8d4ab04
Josselin Chevalay <josselin.chevalay@stochastics.local>
2016-03-06 18:45:40 +0100

    debug projects list and add a filter seach

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -3,7 +3,28 @@
 angular.module('frontendApp')
 .controller('ProjectCtrl', function($TestData, $scope){
     $TestData.Project.get(function(data){
-    	$scope.index = Object.keys(data).length;
     	$scope.projects = data;
+    	$scope.result = data;
     });
+    $scope.search = null;
+    $scope.properties = [
+        "name",
+        "tags"
+    ];
+
+    $scope.getReach = function(){
+    	$scope.result = [];
+
+    	if($scope.search === null){
+    		$scope.result = $scope.projets;
+    		return;
+    	}
+        //$scope.result = _.where($scope.projects, function(row){return name.indexof($scope.search) > -1;});
+        for (var i = $scope.projects.length - 1; i >= 0; i--) {
+        	var field = (typeof $scope.projects[i][$scope.filter] === Array) ? $scope.projects[i][$scope.filter].join(',') :  $scope.projects[i][$scope.filter];
+        	if(field.indexOf($scope.search)>-1){
+        		$scope.result.push($scope.projects[i]);
+        	}
+        }
+    };
 });
