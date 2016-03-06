7cf39ef2fe9192d4599ee6dc40df5742e0f963fe
Josselin Chevalay <josselin.chevalay@stochastics.local>
2016-03-06 19:00:09 +0100

    debug tags reach but rest to debug reverse reach on tag

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -16,12 +16,13 @@ angular.module('frontendApp')
     	$scope.result = [];
 
     	if($scope.search === null){
-    		$scope.result = $scope.projets;
+    		$scope.result = $scope.projects;
     		return;
     	}
         //$scope.result = _.where($scope.projects, function(row){return name.indexof($scope.search) > -1;});
         for (var i = $scope.projects.length - 1; i >= 0; i--) {
-        	var field = (typeof $scope.projects[i][$scope.filter] === Array) ? $scope.projects[i][$scope.filter].join(',') :  $scope.projects[i][$scope.filter];
+        	var field = (typeof $scope.projects[i][$scope.filter] === "object") ? $scope.projects[i][$scope.filter].join(',') :  $scope.projects[i][$scope.filter];;
+        	console.log(field);
         	if(field.indexOf($scope.search)>-1){
         		$scope.result.push($scope.projects[i]);
         	}
