7e531d307c97cbd2bafee5608b56f4773bd1c9b5
Josselin Chevalay <josselin.chevalay@stochastics.local>
2016-03-25 22:40:22 +0100

    add content for project

--- app/scripts/directives/TileProjectDirective.js
+++ app/scripts/directives/TileProjectDirective.js
@@ -6,9 +6,20 @@ angular.module('frontendApp').directive('tileProject', function () { 'use strict
         restrict: 'E',
         replace: true,
         transclude: true,
-        template: '<h3>{{project.name}}</h3>',
+        template: '<div>' + 
+                    '<h3 ng-click="setExpand()">{{project.name}}</h3>' +
+                    '<div ng-show="expand">' +
+                       '<label>{{"description" | translate}} : </label> <span>{{project.description}}<span> <br/>' +
+                       '<label>{{"auteur" | translate}} : </label> <span>{{project.auteur}}<span> <br/>' +
+                       '<label>{{"url" | translate}} : </label> <span>{{project.url}}<span> <br/>' +
+                    '</div>' +
+                   '</div>',
         controller: function ($scope) {
-            
+
+        	$scope.setExpand = function(){
+            	$scope.expand  = ($scope.expand === true) ? false : true;
+            	return;
+            }
         }
     };
 });
