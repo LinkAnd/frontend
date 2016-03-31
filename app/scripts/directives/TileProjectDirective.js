208334b1bddeb55b1e3b44698c36362c6a986647
Josselin Chevalay <josselin.chevalay@stochastics.local>
2016-03-31 18:39:46 +0200

    save

--- app/scripts/directives/TileProjectDirective.js
+++ app/scripts/directives/TileProjectDirective.js
@@ -9,9 +9,8 @@ angular.module('frontendApp').directive('tileProject', function () { 'use strict
         template: '<div>' + 
                     '<h3 ng-click="setExpand()">{{project.name}}</h3>' +
                     '<div ng-show="expand">' +
-                       '<label>{{"description" | translate}} : </label> <span>{{project.description}}<span> <br/>' +
+                       '<label>{{"description" | translate}} : </label> <span class="description">{{project.description}}<span> <br/>' +
                        '<label>{{"auteur" | translate}} : </label> <span>{{project.auteur}}<span> <br/>' +
-                       '<label>{{"url" | translate}} : </label> <span>{{project.url}}<span> <br/>' +
                     '</div>' +
                    '</div>',
         controller: function ($scope) {
